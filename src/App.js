c97ced143db9328ce27fa810b638cefe7041ed0e
OlumideG <olumidegbenjo@gmail.com>
2020-06-07 16:59:11 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () =>{
+   return(
+   <div>
+         <h3> SUBBBBBB </h3>
+         <h3> SUBBBBBB </h3>
+         <h3> SUBBBBBB </h3>
+         <h3> SUBBBBBB </h3>
+         <h3> SUBBBBBB </h3>
+   </div>
+  );
+ }
+ 
+
+  
+  
+
+export default App;
